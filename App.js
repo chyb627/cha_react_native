cae9d1e96a94d5b5432177ad43b4c0f454ca1e1e
cha <chyb627@gmail.com>
2021-12-26 20:53:12 +0900

    리액트 네이티브 시작

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <View className="bg-black h-10 w-10">
+        <Text className="text-3xl font-bold underline"> Hello hi</Text>
+      </View>
+    </SafeAreaView> 
+  );
+};
+export default App;
